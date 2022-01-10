eb00402af8b475a20468c7a618079f886eed1032
Bohdan <bogdandutchak02@gmail.com>
2022-01-10 18:54:21 +0200

    Hope to be final commit!

--- src/test/java/json/JsonObjectAddFindProjectionTest.java
+++ src/test/java/json/JsonObjectAddFindProjectionTest.java
@@ -104,7 +104,7 @@ public class JsonObjectAddFindProjectionTest {
     }
 
     @Test
-    public void testContains() throws Exception{
+    public void testContainsTrue() throws Exception{
         JsonObject jsonObject = new JsonObject(
                 new JsonPair("name", new JsonString("Andrii")),
                 new JsonPair("surname", new JsonString("Rodionov")),
@@ -116,5 +116,20 @@ public class JsonObjectAddFindProjectionTest {
                 )
         );
         assertEquals(true, jsonObject.contains("name"));
+    }
+
+    @Test
+    public void testContainsFalse() throws Exception{
+        JsonObject jsonObject = new JsonObject(
+                new JsonPair("name", new JsonString("Andrii")),
+                new JsonPair("surname", new JsonString("Rodionov")),
+                new JsonPair("year", new JsonNumber(2)),
+                new JsonPair("marks",
+                        new JsonArray(
+                                new JsonNumber(3), new JsonNumber(4), new JsonNumber(2)
+                        )
+                )
+        );
+        assertEquals(false, jsonObject.contains("abc"));
     }
 }
